acdea99a6ccfe579b1ec3f52bbf16b865ad28694
gendeld <gendeld@gmail.com>
2017-09-16 16:54:01 +0300

    Update MTabLayout.java

--- android/src/main/java/com/maornandroidkit/widgets/MTabLayout.java
+++ android/src/main/java/com/maornandroidkit/widgets/MTabLayout.java
@@ -2,6 +2,7 @@ package com.maornandroidkit.widgets;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
