85962e818da51658a1f052571a674b860354028c
gendeld <gendeld@gmail.com>
2017-09-16 16:08:21 +0300

    Update KitsPackage.java

--- android/src/main/java/com/maornandroidkit/KitsPackage.java
+++ android/src/main/java/com/maornandroidkit/KitsPackage.java
@@ -29,7 +29,6 @@ public class KitsPackage implements ReactPackage {
     );
   }
 
-  @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
